629f34b4ecb4ae94aadc0be74d462ebd2f795679
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2024-06-20 02:54:15 +0530

    localize
    ng add @angular/localize
    ng extract-i18n --output-path src/locale
    ng build --localize
    http-server -p 4800

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'internationalisation';
+export class AppComponent implements OnInit {
+  title = $localize`user registration form`;
+
+  internationalForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.internationalForm = this._formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  submit() {
+    console.log(this.internationalForm.value);
+    this.internationalForm.reset();
+  }
 }

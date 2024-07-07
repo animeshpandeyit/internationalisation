cf01207db282235cabb07842dc1998a9450901d0
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2024-07-08 01:08:52 +0530

    start:hi/start:ar
    "start:hi": "ng serve --configuration=hi",
        "start:ar": "ng serve --configuration=ar",
        "build-locale:hi": "ng build --configuration=hi --output-path=dist/hi",
        "build-locale:ar": "ng build --configuration=ar --output-path=dist/ar",

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, LOCALE_ID, Inject } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { I18nService } from '../services/i18n.service';
 import { XliffLoaderService } from '../services/xliff-loader.service';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule,
+  ],
   providers: [I18nService, XliffLoaderService],
 
   templateUrl: './app.component.html',
@@ -17,12 +24,16 @@ import { XliffLoaderService } from '../services/xliff-loader.service';
 })
 export class AppComponent implements OnInit {
   title = $localize`user registration form`;
-
+  languageList = [
+    { code: 'ar', label: 'Arabic' },
+    { code: 'hi', label: 'हिंदी' },
+  ];
   internationalForm: FormGroup;
 
   constructor(
     private _formBuilder: FormBuilder,
-    private i18nService: I18nService
+    private i18nService: I18nService,
+    @Inject(LOCALE_ID) protected localeId: string
   ) {
     this.internationalForm = this._formBuilder.group({
       firstName: [''],
@@ -41,4 +52,6 @@ export class AppComponent implements OnInit {
   switchLanguage(locale: string): void {
     this.i18nService.setLocale(locale);
   }
+
+
 }
